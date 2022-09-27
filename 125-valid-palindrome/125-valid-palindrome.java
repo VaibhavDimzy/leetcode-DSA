f0e24dd3df1ab0827288ef2605c9d61ebe7b153e
VaibhavDimzy <104764035+VaibhavDimzy@users.noreply.github.com>
2022-09-27 20:21:24 +0530

    Time: 19 ms (44.71%), Space: 53.4 MB (15.80%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0) return true;
+        StringBuilder str = new StringBuilder();
+        
+        for(char c: s.toCharArray()) {
+            if((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                str.append(Character.toLowerCase(c));
+            }
+        }
+        System.out.println(str);
+        
+        s = new String(str);
+        if(s.length() == 0) return true;
+        int start=0, end=s.length()-1;
+        return helper(s, start,  end);
+        
+        
+    }
+    public  boolean helper(String s, int start, int end){
+        if(start==end) return true;
+        if(end-start==1) {
+            if(s.charAt(start)== s.charAt(end)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        if(s.charAt(start)==s.charAt(end)){
+            boolean palindrome = helper(s,start+1,end-1);
+            return palindrome;
+        }
+        else
+            return false;
+        
+    }
+}
