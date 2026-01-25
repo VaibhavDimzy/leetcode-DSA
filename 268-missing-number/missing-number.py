fc9b7273a065ae4555e0032a35121ddde79b188f
VaibhavDimzy <104764035+VaibhavDimzy@users.noreply.github.com>
2026-01-25 18:37:27 +0530

    Time: 7 ms (36.60%) | Memory: 20.6 MB (7.03%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,21 +1,32 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        number={}
+
+        #APPROACH 3
+        total=0
+
         for i in nums:
-            if i in number:
-                number[i]+=1
-            else:
-                number[i]=1
+            total = total^i
+        for i in range(len(nums)+1):
+            total=total^i
+
+        return total
+        #APPROACH 2
+        # number={}
+        # for i in nums:
+        #     if i in number:
+        #         number[i]+=1
+        #     else:
+        #         number[i]=1
         
 
 
-        for i in range(len(nums)):
-            if i not in number:
-                return i
+        # for i in range(len(nums)):
+        #     if i not in number:
+        #         return i
 
-        return len(nums)
+        # return len(nums)
                 
-
+        #APPROACH 1
         # for i in range(len(new_arr)):
         #     if i != new_arr[i]:
         #         return i
