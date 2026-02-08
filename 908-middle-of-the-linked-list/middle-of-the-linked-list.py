1a689def51ca2d9916cf5a888ceb7f2fb7cc7712
VaibhavDimzy <104764035+VaibhavDimzy@users.noreply.github.com>
2026-02-08 19:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.21%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,24 +5,41 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) :
-        count=self.getCount(head)
 
-        curr=head
-        for i in range(1,(count//2) + 1):
-            curr=curr.next
+        #APPROACH 2 slow and fast pointers
+
+        slow=head
+        fast=head
+
+        while fast!= None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+
+        return slow
+    
+
+
+
+
+    #     APPRAOCH 1
+    #     count=self.getCount(head)
+
+    #     curr=head
+    #     for i in range(1,(count//2) + 1):
+    #         curr=curr.next
 
         
-        return curr
+    #     return curr
 
 
 
     
-    def getCount(self, head):
-        # code here
-        curr=head
-        count=0
-        while curr!=None:
-            count+=1
-            curr=curr.next
-        return count
+    # def getCount(self, head):
+    #     # code here
+    #     curr=head
+    #     count=0
+    #     while curr!=None:
+    #         count+=1
+    #         curr=curr.next
+    #     return count
         
